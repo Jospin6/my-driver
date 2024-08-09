d7fe129dc5c20c4aa1c713eb6df3132e2dad5e71
jospin6 <jospinndagano1@gmail.com>
2024-08-09 14:19:53 +0200

    Add appbar to the home page

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
 
+  final String title = "Mon Chauffeur";
+
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
@@ -11,10 +13,26 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Stack(
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        backgroundColor: Colors.blue,
+        title: Text(
+          widget.title,
+          style:
+              const TextStyle(color: Colors.white, fontWeight: FontWeight.w500),
+        ),
+        actions: const [
+          Icon(
+            Icons.more_vert,
+            color: Colors.white,
+            size: 25,
+          )
+        ],
+      ),
+      body: const Stack(
         children: [
-          Text("my home page")
+            Text("my home page")
         ],
       )
     );
