4967ae60339011858a889258bc1ba91dd186d5f7
jospin6 <jospinndagano1@gmail.com>
2024-08-09 15:21:58 +0200

    Create vehicul page

---
+++ lib/screens/driver/vehicul_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Vehicul extends StatefulWidget {
+  const Vehicul({super.key});
+
+  @override
+  State<Vehicul> createState() => _VehiculState();
+}
+
+class _VehiculState extends State<Vehicul> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
