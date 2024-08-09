1eda79bd9366c0c02911267aa8a9e6da24a0cf08
jospin6 <jospinndagano1@gmail.com>
2024-08-09 14:40:21 +0200

    Create driver account

---
+++ lib/screens/driver/create_driver_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CreateDriver extends StatefulWidget {
+  const CreateDriver({super.key});
+
+  @override
+  State<CreateDriver> createState() => _CreateDriverState();
+}
+
+class _CreateDriverState extends State<CreateDriver> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
